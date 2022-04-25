c0c1c4a75ae78e586f8329754daaaae68a794143
JuanraCM <juanras89s@gmail.com>
2022-04-25 19:29:53 +0100

    DiscordBot pasa a ser clase estática

--- src/discord_bot.rb
+++ src/discord_bot.rb
@@ -8,49 +8,43 @@ class DiscordBot
 
   COMMANDS_PREFIX = '!'.freeze
 
-  # Constructor principal del bot
-  # Inicializa el cliente y carga los comandos
-  def initialize(opts = {})
-    @production_mode = ENV['RUBY_ENV'] == 'production'
-    @client          = Discordrb::Commands::CommandBot.new token: config.bot_token, prefix: COMMANDS_PREFIX
-
+  # Inicializa el cliente y lanza el proceso
+  def self.run!
+    @client = Discordrb::Commands::CommandBot.new token: credentials.bot_token, prefix: COMMANDS_PREFIX
     read_commands
+
+    @client.run
   end
 
 
-  # Lanza el bot
-  def run
-    @client.run
+  # Devuelve las credendiales del bot, tiene en cuenta el entorno en el que se está ejecutando
+  # para buscar el fichero
+  #
+  # @return [OpenStruct]
+  def self.credentials
+    unless @credentials
+      file_path    = "#{File.dirname(__FILE__)}/credentials_#{ENV['RUBY_ENV']}.yml"
+      @credentials = JSON.parse YAML::load(ERB.new(File.read(file_path)).result).to_json, :object_class => OpenStruct
+    end
+
+    @credentials
+  rescue Errno::ENOENT
+    p "Credentials file missing! path= #{file_path}"
+    exit
   end
 
 
   private
 
     # Procesa los comandos del bot para el cliente
-    def read_commands
+    def self.read_commands
       Commands.constants.each do |command_const|
         command_class = Object.const_get("Commands::#{command_const}")
         command_name  = command_const.to_s.downcase.to_sym
 
         @client.command command_name, command_class.setup do |*args|
-          command_class.new(args, config).execute
+          command_class.new(args, credentials).execute
         end
       end
-    end
-
-
-    # Devuelve la configuración del bot
-    #
-    # @return [OpenStruct]
-    def config
-      unless @config
-        file_path = "#{File.dirname(__FILE__)}/config_#{@production_mode ? 'production' : 'development'}.yml"
-        @config   = JSON.parse YAML::load(ERB.new(File.read(file_path)).result).to_json, :object_class => OpenStruct
-      end
-
-      @config
-    rescue Errno::ENOENT
-      p "Config file missing! path= #{file_path}"
-      exit
     end
 end
